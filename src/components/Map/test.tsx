1a3949493926c6584f6a1ebc3f5e0b1417533931
Thales Henrique <thhenrique@uolinc.com>
2022-07-07 11:37:50 -0300

    test: tests for Map component

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -1,11 +1,40 @@
 import Map from '.'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 describe('<Map />', () => {
   it('should render the correctly', () => {
-    const { getByRole } = render(<Map />)
+    render(<Map />)
 
     // screen.logTestingPlaygroundURL(container)
-    expect(getByRole('link', { name: /openstreetmap/i }))
+    expect(screen.getByRole('link', { name: /openstreetmap/i }))
+  })
+
+  it('should render with the marker in correct place', () => {
+    const places = [
+      {
+        id: '1',
+        name: 'Petrópolis',
+        slug: 'petropolis',
+        location: {
+          latitude: 20,
+          longitude: 30
+        }
+      },
+      {
+        id: '2',
+        name: 'João Pessoa',
+        slug: 'joao_pessoa',
+        location: {
+          latitude: 120,
+          longitude: -50
+        }
+      }
+    ]
+
+    render(<Map places={places} />)
+
+    // screen.logTestingPlaygroundURL(container)
+    expect(screen.getByTitle(/petrópolis/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/joão pessoa/i)).toBeInTheDocument()
   })
 })
